b0667e671e9796c6dc2dbe8d9142a45c77e26ad2
qsctr <brettonchen@gmail.com>
2016-11-11 20:50:40 +0800

    Improve search

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,6 @@
 module Main where
 
-import qualified Data.Set as Set
 import Searches
 
 main :: IO ()
-main = do
-  putStrLn "hello world"
-  print $ map coords <$> ucs (0, 0) (99, 204) Set.empty
+main = undefined
--- src/Searches.hs
+++ src/Searches.hs
@@ -1,10 +1,15 @@
-module Searches (ucs, Node (..)) where
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+
+module Searches (search, SearchType (..)) where
 
 import Data.Function
 import qualified Data.PQueue.Min as PQ
 import qualified Data.Set as Set
 
 type Coords = (Int, Int)
+type SearchInfo = (SearchType, Coords, Coords, Set.Set Node)
+type SearchData = (PQ.MinQueue PQNode, Set.Set Node)
+type SearchResult = Either (Coords, [Coords], SearchData) (Maybe [Coords])
 
 data Node = Node { coords :: Coords, score :: Double, parent :: Maybe Node }
 instance Eq Node where (==) = (==) `on` coords
@@ -14,28 +19,39 @@ newtype PQNode = PQNode { unPQNode :: Node }
 instance Eq PQNode where (==) = (==) `on` score . unPQNode
 instance Ord PQNode where compare = compare `on` score . unPQNode
 
-ucs :: Coords -> Coords -> Set.Set Node -> Maybe [Node]
-ucs start goal walls = ucs' (PQ.singleton $ PQNode $ Node start 0 Nothing) Set.empty
-  where
-    ucs' fringe expanded = case PQ.minView fringe of
-      Nothing -> Nothing
-      Just ((PQNode current), fringe') ->
-        if Set.member current expanded then
-          ucs' fringe' expanded
-        else if coords current == goal then
-          let buildPath path = case parent (head path) of
-                Just node -> buildPath $ node : path
-                Nothing -> path
-          in  Just $ buildPath [current]
-        else
-          let expanded' = Set.insert current expanded
-              fringe'' = PQ.union fringe' $ PQ.fromList
-                [ PQNode child | a <- [-1..1], b <- [-1..1], (a, b) /= (0, 0)
-                , let childCoords = (fst (coords current) + a, snd (coords current) + b)
-                , let childScore = score current + distance childCoords (coords current)
-                , let child = Node childCoords childScore $ Just current
-                , Set.notMember child walls, Set.notMember child expanded' ]
-          in  ucs' fringe'' expanded'
+data SearchType = UniformCost | Greedy | AStar
+
+search :: SearchInfo -> Maybe SearchData -> SearchResult
+search info@(searchType, start, goal, walls) searchData = case searchData of
+  Nothing ->
+    let initScore = case searchType of
+          UniformCost -> 0
+          _ -> distance start goal
+    in  search info $ Just (PQ.singleton $ PQNode $ Node start initScore Nothing, Set.empty)
+  Just (fringe, expanded) -> case PQ.minView fringe of
+    Nothing -> Right Nothing
+    Just ((PQNode current), fringe') ->
+      if Set.member current expanded then
+          search info $ Just (fringe', expanded)
+      else if coords current == goal then
+        let buildPath prev path = case parent prev of
+              Just node -> buildPath node $ coords prev : path
+              Nothing -> coords prev : path
+        in  Right $ Just $ buildPath current []
+      else
+        let childNodes = [ child | a <- [-1..1], b <- [-1..1], (a, b) /= (0, 0)
+              , let childCoords = (fst (coords current) + a, snd (coords current) + b)
+                    childScore = case searchType of
+                      UniformCost -> uniformCost
+                      Greedy -> greedy
+                      AStar -> uniformCost + greedy
+                    uniformCost = score current + distance childCoords (coords current)
+                    greedy = distance childCoords goal
+                    child = Node childCoords childScore $ Just current
+              , Set.notMember child walls, Set.notMember child expanded' ]
+            fringe'' = PQ.union fringe' $ PQ.fromList $ map PQNode childNodes
+            expanded' = Set.insert current expanded
+        in  Left (coords current, map coords childNodes, (fringe'', expanded'))
 
 distance :: Coords -> Coords -> Double
 distance (x1, y1) (x2, y2) = sqrt $ fromIntegral $ (y2 - y1) ^ 2 + (x2 - x1) ^ 2
